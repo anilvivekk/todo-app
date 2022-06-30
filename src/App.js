4358ea0881eb3db360e506d7c82653f626cbbeb9
anilvivekk <avanilvivek056@gmail.com>
2022-06-30 10:47:28 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,23 @@ function App() {
   const [status, setStatus] = useState("All");
   const [filteredTodos, setFilteredTodos] = useState([]);
 
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+const filterHandler = () => {
+  switch(status){
+    case "completed":
+      setFilteredTodos(todos.filter(todo => todo.completed === true));
+      break;
+      case "uncompleted" :
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+  }
+};
   //use only once
   useEffect(() => {
     getLocalTodos();
@@ -20,24 +37,10 @@ function App() {
     saveLocalTodos();
   }, [todos, status]);
 
-  const filterHandler = () => {
-    switch(status){
-      case "completed":
-        setFilteredTodos(todos.filter(todo => todo.completed === true));
-        break;
-        case "uncompleted" :
-          setFilteredTodos(todos.filter(todo => todo.completed === false));
-          break;
-        default:
-          setFilteredTodos(todos);
-          break;
-    }
-  };
+  
   // local save
 
-  const saveLocalTodos = () => {
-      localStorage.setItem('todos', JSON.stringify(todos));
-  }
+  
   const getLocalTodos = () => {
     if(localStorage.getItem('todos') === null){
       localStorage.setItem('todos', JSON.stringify([]));
